12757c324f8d2f38a4010b35726099aa2a4dbdf3
Jonas Dann <jonas.chr.dann@gmail.com>
2016-12-10 00:57:24 +0100

    Added pew pew

---
+++ Assets/Look.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Look : MonoBehaviour {
+    public int lookSpeed = 15;
+    public Object bullet;
+
+    // Use this for initialization
+	void Start () {
+	    Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    var mov = new Vector3(transform.localEulerAngles.x - Input.GetAxis("Mouse Y"), transform.localEulerAngles.y + Input.GetAxis("Mouse X"), 0);
+	    transform.localEulerAngles = mov;
+
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        var gun = gameObject.transform.Find("gun").gameObject;
+	        GameObject bulletInstance = (GameObject) Instantiate(bullet);
+	        bulletInstance.transform.position = gun.transform.position;
+	        bulletInstance.transform.rotation = gun.transform.rotation;
+            bulletInstance.transform.Rotate(new Vector3(0, 1, 0), -90);
+	    }
+	}
+}
---
+++ Assets/MoveBullet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MoveBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(new Vector3(0, 0, 1));
+	}
+}
